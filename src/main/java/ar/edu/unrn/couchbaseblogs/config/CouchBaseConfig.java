49a2a3e96bb8e609adbda5f1c05b5d6912b2a51f
Mateo <maliberti@unrn.edu.ar>
2025-04-09 10:17:05 -0300

    modify(config): moved converters to couchbaseconfig class

--- src/main/java/ar/edu/unrn/couchbaseblogs/config/CouchBaseConfig.java
+++ src/main/java/ar/edu/unrn/couchbaseblogs/config/CouchBaseConfig.java
@@ -3,10 +3,18 @@ package ar.edu.unrn.couchbaseblogs.config;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.ClusterOptions;
 import com.couchbase.client.java.env.ClusterEnvironment;
+import lombok.Locked;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.convert.WritingConverter;
 import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
+import org.springframework.data.couchbase.core.convert.CustomConversions;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @Configuration
 public class CouchBaseConfig extends AbstractCouchbaseConfiguration {
@@ -36,10 +44,34 @@ public class CouchBaseConfig extends AbstractCouchbaseConfiguration {
   public CouchBaseConfig(@Qualifier("couchbaseEnvironment") ClusterEnvironment environment) {
       this.environment = environment;
   }
+  @Override
+  public CustomConversions customConversions() {
+    return new CustomConversions(Arrays.asList(LocalDateToStringConverter.INSTANCE, StringToLocalDateConverter.INSTANCE));
+  }
+
 
   @Bean
   public Cluster couchBaseCluster(){
     return Cluster.connect(getConnectionString(),
             ClusterOptions.clusterOptions(getUserName(), getPassword()).environment(environment));
   }
+
+  @WritingConverter
+  public static enum LocalDateToStringConverter implements Converter<LocalDate, String> {
+    INSTANCE;
+
+    @Override
+    public String convert(LocalDate source) {
+      return source.toString(); // Convierte LocalDate a "yyyy-MM-dd"
+    }
+  }
+  @ReadingConverter
+  public static enum StringToLocalDateConverter implements Converter<String, LocalDate> {
+    INSTANCE;
+    @Override
+    public LocalDate convert(String source) {
+      return LocalDate.parse(source); // Convierte String a LocalDate
+    }
+  }
+
 }
