68e760963fab22fc53602e0dcd959fdd10d8571d
Mateo <maliberti@unrn.edu.ar>
2025-04-09 10:16:34 -0300

    modify(model): added document annotation on model entities

--- src/main/java/ar/edu/unrn/couchbaseblogs/model/Page.java
+++ src/main/java/ar/edu/unrn/couchbaseblogs/model/Page.java
@@ -3,18 +3,25 @@ package ar.edu.unrn.couchbaseblogs.model;
 import java.time.LocalDate;
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.couchbase.core.mapping.Document;
+import org.springframework.data.couchbase.core.mapping.Field;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Document
 public class Page {
     @Id
     private String id;
+    @Field
     private String title;
+    @Field
     private String author;
+    @Field
     private String text;
+    @Field
     private LocalDate date;
 
 }
--- src/main/java/ar/edu/unrn/couchbaseblogs/model/Post.java
+++ src/main/java/ar/edu/unrn/couchbaseblogs/model/Post.java
@@ -5,20 +5,30 @@ import java.time.LocalDate;
 import java.util.List;
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.couchbase.core.mapping.Document;
+import org.springframework.data.couchbase.core.mapping.Field;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Document
 public class Post {
     @Id
     private String id;
+    @Field
     private String title;
+    @Field
     private String author;
+    @Field
     private String text;
+    @Field
     private List<String> tags;
+    @Field
     private String resume;
+    @Field
     private List<String> relatedLinks;
+    @Field
     private LocalDate date;
 }
